e702e5305df3e114d22fc83af25509e417478c48
doronkina <liudmyla.doronkina0@gmail.com>
2020-04-17 00:18:06 +0300

    Updates

---
+++ precache-manifest.98c80fe39ee45b4f82c7d23e9a20b283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2c1a9574e9efd3c3cf99e86c562915",
+    "url": "/game-in-dots/index.html"
+  },
+  {
+    "revision": "c514219ff5ab268e5fb2",
+    "url": "/game-in-dots/static/css/main.0479eff6.chunk.css"
+  },
+  {
+    "revision": "4e418dfca52e84acf422",
+    "url": "/game-in-dots/static/js/2.8dfdfdf6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-in-dots/static/js/2.8dfdfdf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c514219ff5ab268e5fb2",
+    "url": "/game-in-dots/static/js/main.45f4ab29.chunk.js"
+  },
+  {
+    "revision": "a12afa66e93e99f07a06",
+    "url": "/game-in-dots/static/js/runtime-main.ee05fabd.js"
+  }
+]);
